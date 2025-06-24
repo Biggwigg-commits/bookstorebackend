c453056624f4527f19dd3e7a78662a1177ea6440
Biggwigg-commits <soquick2006@gmail.com>
2025-06-24 23:24:20 +0530

    Update server.py

--- server.py
+++ server.py
@@ -90,7 +90,7 @@ sample_books = [
         "category": "Young Readers",
         "description": "A thrilling mystery adventure as Bubble Bear solves the case of the mysterious Pumpkin Pirate. Perfect for young readers who love mysteries and adventures.",
         "price": 12.99,
-        "image_url": "https://blogger.googleusercontent.com/img/a/AVvXsEgiFiLXUg3WkITwf_2znvnAUQH3O_7-C9SXH7CFVYnF9nxWHxNAbmLlnzF09-EskP_B5JZVsadhm9phyWEnHjwy_OSIINS056TFdpyyW0zkVX3McxTbTew3mTGNcm2ladKznT3J406oSmPgtjfTUQQ7BC5CKCMxOD5iIpIQ8hKiLEbspPLPmfeon36z-wc",
+        "image_url": "https://i.ibb.co/4Z5cv9rV/1a7d4246-702f-488f-bd88-4761f0f37e10.jpg",
         "amazon_link": "https://www.amazon.com/s?k=Bubble+Bear+Mystery+Pumpkin+Pirate+Garry+Jordan",
         "featured": False
     },
